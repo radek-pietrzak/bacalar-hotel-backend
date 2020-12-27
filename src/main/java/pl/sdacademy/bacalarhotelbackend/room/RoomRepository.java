19ed3c1149e1066808d47be41ba402534350c27e
radek <radekpietrzak102@gmail.com>
2020-12-27 18:14:33 +0100

    create room repository interface

--- src/main/java/pl/sdacademy/bacalarhotelbackend/room/RoomRepository.java
+++ src/main/java/pl/sdacademy/bacalarhotelbackend/room/RoomRepository.java
@@ -1,4 +1,6 @@
 package pl.sdacademy.bacalarhotelbackend.room;
 
-public interface RoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
