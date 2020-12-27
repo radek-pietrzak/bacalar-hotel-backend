2e7d343482774d48ca74f5683002c004aca1d9b7
radek <radekpietrzak102@gmail.com>
2020-12-27 17:44:22 +0100

    Initial commit

---
+++ src/main/java/pl/sdacademy/bacalarhotelbackend/BacalarHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.bacalarhotelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacalarHotelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacalarHotelBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sdacademy/bacalarhotelbackend/BacalarHotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.bacalarhotelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacalarHotelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
