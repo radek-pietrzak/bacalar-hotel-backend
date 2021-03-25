c44b7f310aea2907a1da1a690c11082a27329be1
radek <radekpietrzak102@gmail.com>
2021-03-25 11:08:12 +0100

    room patch controllers fixed

--- src/main/java/pl/sdacademy/bacalarhotelbackend/room/RoomController.java
+++ src/main/java/pl/sdacademy/bacalarhotelbackend/room/RoomController.java
@@ -51,36 +51,47 @@ public class RoomController {
 
     @Transactional
     @PatchMapping("/{id}")
-    public ResponseEntity<?> patchRoomNumber(@PathVariable long id, @RequestBody int roomNumberToUpdate) {
+    public ResponseEntity<Room> patchRoomNumber(@PathVariable long id, @RequestBody int roomNumberToUpdate) {
         if (!roomRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        roomRepository.getOne(id).setRoomNumber(roomNumberToUpdate);
-        return ResponseEntity.ok().build();
+        roomRepository.findById(id)
+                .ifPresent(r -> r.setRoomNumber(roomNumberToUpdate));
+        return roomRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @Transactional
     @PatchMapping("/{id}/single-bed-change")
-    public ResponseEntity<?> patchNumberOfSingleBed(@PathVariable long id, @RequestBody int numberOfSingleBedToUpdate) {
+    public ResponseEntity<Room> patchNumberOfSingleBed(@PathVariable long id, @RequestBody int numberOfSingleBedToUpdate) {
         if (!roomRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        roomRepository.getOne(id).setNumberOfSingleBed(numberOfSingleBedToUpdate);
-        roomRepository.getOne(id).setNumberOfPerson(numberOfSingleBedToUpdate
-                + roomRepository.getOne(id).getNumberOfDoubleBed() * 2);
-        return ResponseEntity.ok().build();
+        roomRepository.findById(id)
+                .ifPresent(s -> {
+                    s.setNumberOfSingleBed(numberOfSingleBedToUpdate);
+                    s.setNumberOfPerson(numberOfSingleBedToUpdate + s.getNumberOfDoubleBed() * 2);
+                });
+        return roomRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @Transactional
     @PatchMapping("/{id}/double-bed-change")
-    public ResponseEntity<?> patchNumberOfDoubleBed(@PathVariable long id, @RequestBody int numberOfDoubleBedToUpdate) {
+    public ResponseEntity<Room> patchNumberOfDoubleBed(@PathVariable long id, @RequestBody int numberOfDoubleBedToUpdate) {
         if (!roomRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        roomRepository.getOne(id).setNumberOfDoubleBed(numberOfDoubleBedToUpdate);
-        roomRepository.getOne(id).setNumberOfPerson(numberOfDoubleBedToUpdate * 2
-                + roomRepository.getOne(id).getNumberOfSingleBed());
-        return ResponseEntity.ok().build();
+        roomRepository.findById(id)
+                .ifPresent(d -> {
+                    d.setNumberOfDoubleBed(numberOfDoubleBedToUpdate);
+                    d.setNumberOfPerson(d.getNumberOfSingleBed() + numberOfDoubleBedToUpdate * 2);
+                });
+        return roomRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
